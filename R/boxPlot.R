1a21096f52f45bba77abf6b831713687f34dcf18
Blocktt <68349347+Blocktt@users.noreply.github.com>
2024-02-12 11:27:43 -0500

    v0.0.1.9060
    Made major changes to boxPlot function following Hannah's edits. Need to remove Log10 version now that it's redundant.

--- R/boxPlot.R
+++ R/boxPlot.R
@@ -1,17 +1,18 @@
 #' WQ boxplot.
 #'
-#' The boxPlot function produces boxplots for WQ data depending on an assessment
-#'  unit identifier (AU_ID) and water quality constituent (TADA.CharacteristicName).
-#'  Multiple values can be entered into either argument.
+#' The boxPlot function produces boxplots for WQ data depending on
+#' an assessment unit identifier (AU_ID). Each WQ characteristic is plotted separately.
+#' The y-axis can be Log10 scaled using a TRUE/FALSE argument.
 #'
 #' @import magrittr
 #'
 #' @param data A WQ dataset in long format. Must contain either AU_ID or TADA.CharacteristicName
+#' @param WQS_table Water quality standards crosswalk table
 #' @param AU_ID An AK DEC Assessment Unit (AU) Identifier
-#' @param constituent TADA.CharacteristicName as generated after running data_processing.R
+#' @param y_axis_log A TRUE/FALSE argument that specifies whether the Y-axis should be Log10 scaled
 #'
 #' @examples
-#' # Examples 1-4, data from Excel
+#' # Example, data from Excel
 #'\dontrun{
 #' # Packages
 #' library(readr) # readr is a tidyverse package to read CSVs
@@ -20,118 +21,94 @@
 #'                                        , package = "AKDECtools")
 #'                             , guess_max = 10^6)
 #'
+#' df_WQS_Crosswalk <- read_csv(system.file("extdata/AK_WQS_Crosswalk.csv"
+#'                                        , package = "AKDECtools")
+#'                             , guess_max = 10^6)
+#'
 #' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' # Example 1 - single AU and single constituent
 #'
-#' boxPlot(data = df_ExampSamps, AU_ID = c('AK_R_1010504_005')
-#' , constituent = c('PH'))
+#' boxPlot(data = df_ExampSamps, WQS_table = df_WQS_Crosswalk
+#' , AU_ID = c('AK_R_1010504_005'), y_axis_log = F)
 #'
-#' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' # Example 2 - multiple AUs and single constituent
-#'
-#' boxPlot(data = df_ExampSamps, AU_ID = c('AK_R_1010504_005', 'AK_B_1010203_001')
-#' , constituent = c('TEMPERATURE, WATER'))
-#'
-#' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' # Example 3 - single AU and multiple constituents
-#'
-#' boxPlot(data = df_ExampSamps, AU_ID = c('AK_R_1010504_005')
-#' , constituent = c('PH', 'TEMPERATURE, WATER'))
-#'
-#' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' # Example 4 - multiple AUs and multiple constituents
-#'
-#' boxPlot(data = df_ExampSamps, AU_ID = c('AK_R_1010504_005', 'AK_B_1010203_001')
-#' , constituent = c('PH', 'TEMPERATURE, WATER'))
 #' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' }
 #'
 #' @return A boxplot
 #' @export
 #'
-boxPlot <- function(data, AU_ID, constituent) {
+boxPlot <- function(data, WQS_table, AU_ID, y_axis_log) {
 
-  if(missing(AU_ID)) {
-    filt <- data %>% dplyr::filter(TADA.CharacteristicName %in% constituent)
-  } else if(missing(constituent)) {
-    filt <- data %>% dplyr::filter(AUID_ATTNS %in% AU_ID)
-  } else {
-    filt <- data %>% dplyr::filter(AUID_ATTNS %in% AU_ID) %>%
-      dplyr::filter(TADA.CharacteristicName %in% constituent)
+  relevant_constituents <- WQS_table %>%
+    dplyr::select(TADA.Constituent) %>%
+    unique() %>%
+    dplyr::pull()
+
+  relevant_data <- data %>%
+    dplyr::filter(TADA.CharacteristicName %in% relevant_constituents) %>%
+    dplyr::filter(AUID_ATTNS == AU_ID)
+
+  constituents <- relevant_data %>%
+    dplyr::select(TADA.CharacteristicName) %>%
+    unique() %>%
+    dplyr::pull()
+
+  results <- list()
+  counter <- 0
+  #Loop through constituents
+  for(j in constituents) {
+
+    counter<- counter+1
+
+    filt <- relevant_data %>%
+      dplyr::filter(TADA.CharacteristicName == j)
+
+    if(y_axis_log == F) {
+      plt<-ggplot2::ggplot() +
+        ggplot2::geom_boxplot(data = filt,
+                              aes(x = AUID_ATTNS,
+                                  y = TADA.ResultMeasureValue),
+                              color = 'gray30') +
+        ggplot2::geom_jitter(data = filt, aes(x = AUID_ATTNS,
+                                              y = TADA.ResultMeasureValue,
+                                              fill = MonitoringLocationIdentifier),
+                             color = 'black',
+                             shape = 21,
+                             size = 2,
+                             alpha = 0.8) +
+        ggplot2::xlab('AU ID') +
+        ggplot2::ylab(paste0(j, ' (', filt$TADA.ResultMeasure.MeasureUnitCode, ')')) +
+        ggplot2::theme_bw() +
+        viridis::scale_fill_viridis(discrete = T,
+                                    option = "mako") +
+        ggplot2::labs(fill = 'Monitoring Location') +
+        ggplot2::theme(legend.position="top")
+
+      results[[counter]] <- plt
+    } else {
+      plt<-ggplot2::ggplot() +
+        ggplot2::geom_boxplot(data = filt,
+                              aes(x = AUID_ATTNS,
+                                  y = TADA.ResultMeasureValue),
+                              color = 'gray30') +
+        ggplot2::geom_jitter(data = filt, aes(x = AUID_ATTNS,
+                                              y = TADA.ResultMeasureValue,
+                                              fill = MonitoringLocationIdentifier),
+                             color = 'black',
+                             shape = 21,
+                             size = 2,
+                             alpha = 0.8) +
+        ggplot2::xlab('AU ID') +
+        ggplot2::ylab(paste0(j, ' (', filt$TADA.ResultMeasure.MeasureUnitCode, ')')) +
+        ggplot2::scale_y_log10() +
+        ggplot2::theme_bw() +
+        viridis::scale_fill_viridis(discrete = T,
+                                    option = "mako") +
+        ggplot2::labs(fill = 'Monitoring Location') +
+        ggplot2::theme(legend.position="top")
+
+      results[[counter]] <- plt
+    }
   }
-
-  val_name <- filt %>% dplyr::select(TADA.CharacteristicName) %>% unique() %>% nrow()
-
-
-  if(length(AU_ID) == 1 & length(constituent) == 1) {
-    plt<-ggplot2::ggplot() +
-      ggplot2::geom_boxplot(data = filt, ggplot2::aes(x = AUID_ATTNS,
-                                    y = TADA.ResultMeasureValue),
-                   color = 'black') +
-      ggplot2::geom_jitter(data = filt, ggplot2::aes(x = AUID_ATTNS,
-                                   y = TADA.ResultMeasureValue)) +
-      ggplot2::xlab('AU ID') +
-      ggplot2::ylab(ifelse(val_name > 1, 'Result', paste0(filt$TADA.CharacteristicName
-                                                 , ' ('
-                                                 , filt$TADA.ResultMeasure.MeasureUnitCode
-                                                 , ')'))) +
-      ggplot2::theme_classic() +
-      ggplot2::theme(legend.position = "none")
-  } else if (length(AU_ID) > 1 & length(constituent) == 1){
-    plt<-ggplot2::ggplot() +
-      ggplot2::geom_boxplot(data = filt, ggplot2::aes(x = AUID_ATTNS,
-                                    y = TADA.ResultMeasureValue),
-                   color = 'black') +
-      ggplot2::geom_jitter(data = filt, ggplot2::aes(x = AUID_ATTNS,
-                                   y = TADA.ResultMeasureValue)) +
-      ggplot2::xlab('AU ID') +
-      ggplot2::ylab(ifelse(val_name > 1, 'Result', paste0(filt$TADA.CharacteristicName
-                                                 , ' ('
-                                                 , filt$TADA.ResultMeasure.MeasureUnitCode
-                                                 , ')'))) +
-      ggplot2::theme_classic() +
-      ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
-            panel.grid.minor = ggplot2::element_blank(),
-            strip.background = ggplot2::element_rect(fill = 'gray95'),
-            legend.position = "none")
-  } else if(length(AU_ID) == 1 & length(constituent) > 1){
-    plt<-ggplot2::ggplot() +
-      ggplot2::geom_boxplot(data = filt, ggplot2::aes(x = AUID_ATTNS,
-                                    y = TADA.ResultMeasureValue),
-                   color = 'black') +
-      ggplot2::geom_jitter(data = filt, ggplot2::aes(x = AUID_ATTNS,
-                                   y = TADA.ResultMeasureValue)) +
-      ggplot2::facet_wrap(~TADA.CharacteristicName, scales = 'free_y') +
-      ggplot2::xlab('AU ID') +
-      ggplot2::ylab(ifelse(val_name > 1, 'Result', paste0(filt$TADA.CharacteristicName
-                                                 , ' ('
-                                                 , filt$TADA.ResultMeasure.MeasureUnitCode
-                                                 , ')'))) +
-      ggplot2::theme_bw() +
-      ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
-            panel.grid.minor = ggplot2::element_blank(),
-            strip.background = ggplot2::element_rect(fill = 'gray95'),
-            legend.position = "none")
-  } else {
-    plt<-ggplot2::ggplot() +
-      ggplot2::geom_boxplot(data = filt, ggplot2::aes(x = AUID_ATTNS,
-                                    y = TADA.ResultMeasureValue),
-                   color = 'black') +
-      ggplot2::geom_jitter(data = filt, ggplot2::aes(x = AUID_ATTNS,
-                                   y = TADA.ResultMeasureValue)) +
-      ggplot2::facet_wrap(~TADA.CharacteristicName, scales = 'free_y') +
-      ggplot2::xlab('AU ID') +
-      ggplot2::ylab(ifelse(val_name > 1, 'Result', paste0(filt$TADA.CharacteristicName
-                                                 , ' ('
-                                                 , filt$TADA.ResultMeasure.MeasureUnitCode
-                                                 , ')'))) +
-      ggplot2::theme_bw() +
-      ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
-            panel.grid.minor = ggplot2::element_blank(),
-            strip.background = ggplot2::element_rect(fill = 'gray95'),
-            axis.text.x = ggplot2::element_text(angle = 45, hjust=1),
-            legend.position = "none")
-  }
-
-  return(plt)
+  return(results)
 } # end if/else
