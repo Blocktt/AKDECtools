0d92a20140ac211aec625773966d3e9359e1a201
Blocktt <68349347+Blocktt@users.noreply.github.com>
2024-02-12 11:14:07 -0500

    v0.0.1.9050
    Updates to timeSeries function from Hannah. Need to remove log10 version as this is now an argument in timeSeries itself.

--- R/timeSeriesPlot.R
+++ R/timeSeriesPlot.R
@@ -1,17 +1,18 @@
 #' WQ time series plotting.
 #'
 #' The timeSeries function produces time series plots for WQ data depending on
-#' an assessment unit identifier (AU_ID) and water quality constituent
-#' (TADA.CharacteristicName). Multiple values can be entered into either argument.
+#' an assessment unit identifier (AU_ID). Each WQ characteristic is plotted separately.
+#' The y-axis can be Log10 scaled using a TRUE/FALSE argument.
 #'
 #' @import magrittr
 #'
 #' @param data A WQ dataset in long format. Must contain either AU_ID or TADA.CharacteristicName
+#' @param WQS_table Water quality standards crosswalk table
 #' @param AU_ID An AK DEC Assessment Unit (AU) Identifier
-#' @param constituent TADA.CharacteristicName as generated after running data_processing.R
+#' @param y_axis_log A TRUE/FALSE argument that specifies whether the Y-axis should be Log10 scaled
 #'
 #' @examples
-#' # Examples 1-4, data from Excel
+#' # Example, data from Excel
 #'\dontrun{
 #' # Packages
 #' library(readr) # readr is a tidyverse package to read CSVs
@@ -20,121 +21,89 @@
 #'                                        , package = "AKDECtools")
 #'                             , guess_max = 10^6)
 #'
-#' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' # Example 1 - single AU and single constituent
-#'
-#' timeSeries(data = df_ExampSamps, AU_ID = c('AK_R_1010504_005')
-#' , constituent = c('PH'))
+#' df_WQS_Crosswalk <- read_csv(system.file("extdata/AK_WQS_Crosswalk.csv"
+#'                                        , package = "AKDECtools")
+#'                             , guess_max = 10^6)
 #'
 #' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' # Example 2 - multiple AUs and single constituent
+#' # Example 1
 #'
-#' timeSeries(data = df_ExampSamps, AU_ID = c('AK_R_1010504_005', 'AK_B_1010203_001')
-#' , constituent = c('TEMPERATURE, WATER'))
+#' timeSeries(data = df_ExampSamps, WQS_table = df_WQS_Crosswalk
+#' , AU_ID = c('AK_R_1010504_005'), y_axis_log = F)
 #'
-#' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' # Example 3 - single AU and multiple constituents
-#'
-#' timeSeries(data = df_ExampSamps, AU_ID = c('AK_R_1010504_005')
-#' , constituent = c('PH', 'TEMPERATURE, WATER'))
-#'
-#' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' # Example 4 - multiple AUs and multiple constituents
-#'
-#' timeSeries(data = df_ExampSamps, AU_ID = c('AK_R_1010504_005', 'AK_B_1010203_001')
-#' , constituent = c('PH', 'TEMPERATURE, WATER'))
 #' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' }
 #'
-#' @return A plot
+#' @return A time series plot
 #' @export
 #'
-timeSeries <- function(data, AU_ID, constituent) {
+timeSeries <- function(data, WQS_table, AU_ID, y_axis_log) {
 
-  if(missing(AU_ID)) {
-    filt <- data %>% dplyr::filter(TADA.CharacteristicName %in% constituent)
-  } else if(missing(constituent)) {
-    filt <- data %>% dplyr::filter(AUID_ATTNS %in% AU_ID)
-  } else {
-    filt <- data %>% dplyr::filter(AUID_ATTNS %in% AU_ID) %>%
-      dplyr::filter(TADA.CharacteristicName %in% constituent)
+  relevant_constituents <- WQS_table %>%
+    dplyr::select(TADA.Constituent) %>%
+    unique() %>%
+    dplyr::pull()
+
+  relevant_data <- data %>%
+    dplyr::filter(TADA.CharacteristicName %in% relevant_constituents) %>%
+    dplyr::filter(AUID_ATTNS == AU_ID)
+
+  constituents <- relevant_data %>%
+    dplyr::select(TADA.CharacteristicName) %>%
+    unique() %>%
+    dplyr::pull()
+
+  results <- list()
+  counter <- 0
+  #Loop through constituents
+  for(j in constituents) {
+
+    counter<- counter+1
+
+    filt <- relevant_data %>%
+      dplyr::filter(TADA.CharacteristicName == j)
+
+    if(y_axis_log == F) {
+      plt<-ggplot2::ggplot() +
+        ggplot2::geom_point(data = filt,
+                            aes(x = ActivityStartDate,
+                                y = TADA.ResultMeasureValue,
+                                fill = MonitoringLocationIdentifier),
+                            color = 'black',
+                            shape = 21,
+                            size = 2,
+                            alpha = 0.8) +
+        ggplot2::xlab('Time') +
+        ggplot2::ylab(paste0(j, ' (', filt$TADA.ResultMeasure.MeasureUnitCode, ')')) +
+        ggplot2::theme_bw() +
+        viridis::scale_fill_viridis(discrete = T,
+                                    option = "mako") +
+        ggplot2::labs(fill = 'Monitoring Location') +
+        ggplot2::theme(legend.position="top")
+
+      results[[counter]] <- plt
+    } else {
+      plt<-ggplot2::ggplot() +
+        ggplot2::geom_point(data = filt,
+                            aes(x = ActivityStartDate,
+                                y = TADA.ResultMeasureValue,
+                                fill = MonitoringLocationIdentifier),
+                            color = 'black',
+                            shape = 21,
+                            size = 2,
+                            alpha = 0.8) +
+        ggplot2::xlab('Time') +
+        ggplot2::scale_y_log10() +
+        ggplot2::ylab(paste0(j, ' (', filt$TADA.ResultMeasure.MeasureUnitCode, ')')) +
+        ggplot2::theme_bw() +
+        viridis::scale_fill_viridis(discrete = T,
+                                    option = "mako") +
+        ggplot2::labs(fill = 'Monitoring Location') +
+        ggplot2::theme(legend.position="top")
+
+      results[[counter]] <- plt
+    }
   }
-
-  val_name <- filt %>% dplyr::select(TADA.CharacteristicName) %>% unique() %>% nrow()
-
-
-  if(length(AU_ID) == 1 & length(constituent) == 1) {
-    plt<-ggplot2::ggplot() +
-      ggplot2::geom_point(data = filt, ggplot2::aes(x = ActivityStartDate,
-                                  y = TADA.ResultMeasureValue),
-                 color = 'black',
-                 size = 2, alpha = 0.8) +
-      ggplot2::xlab('Time') +
-      ggplot2::ylab(ifelse(val_name > 1, 'Result', paste0(filt$TADA.CharacteristicName
-                                                 , ' ('
-                                                 , filt$TADA.ResultMeasure.MeasureUnitCode
-                                                 , ')'))) +
-      ggplot2::ggtitle(paste0(AU_ID)) +
-      ggplot2::theme_classic() +
-      ggplot2::theme(legend.position="none")
-
-  } else if (length(AU_ID) > 1 & length(constituent) == 1){
-    plt<-ggplot2::ggplot() +
-      ggplot2::geom_point(data = filt, ggplot2::aes(x = ActivityStartDate,
-                                  y = TADA.ResultMeasureValue),
-                 color = 'black',
-                 size = 2, alpha = 0.8) +
-      ggplot2::facet_wrap(~AUID_ATTNS) +
-      ggplot2::xlab('Time') +
-      ggplot2::ylab(ifelse(val_name > 1, 'Result', paste0(filt$TADA.CharacteristicName
-                                                 , ' ('
-                                                 , filt$TADA.ResultMeasure.MeasureUnitCode
-                                                 , ')'))) +
-      ggplot2::theme_bw() +
-      ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
-            panel.grid.minor = ggplot2::element_blank(),
-            strip.background = ggplot2::element_rect(fill = 'gray95'),
-            legend.position="none")
-
-  } else if(length(AU_ID) == 1 & length(constituent) > 1){
-    plt<-ggplot2::ggplot() +
-      ggplot2::geom_point(data = filt, ggplot2::aes(x = ActivityStartDate,
-                                  y = TADA.ResultMeasureValue),
-                 color = 'black',
-                 size = 2, alpha = 0.8) +
-      ggplot2::facet_wrap(~TADA.CharacteristicName, scale = "free_y") +
-      ggplot2::xlab('Time') +
-      ggplot2::ylab(ifelse(val_name > 1, 'Result', paste0(filt$TADA.CharacteristicName
-                                                 , ' ('
-                                                 , filt$TADA.ResultMeasure.MeasureUnitCode
-                                                 , ')'))) +
-      ggplot2::ggtitle(paste0(AU_ID)) +
-      ggplot2::theme_bw() +
-      ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
-            panel.grid.minor = ggplot2::element_blank(),
-            strip.background = ggplot2::element_rect(fill = 'gray95'),
-            legend.position="none")
-
-  } else {
-    plt<-ggplot2::ggplot() +
-      ggplot2::geom_point(data = filt, ggplot2::aes(x = ActivityStartDate,
-                                  y = TADA.ResultMeasureValue),
-                 color = 'black',
-                 size = 2, alpha = 0.8) +
-      ggplot2::facet_grid(cols = ggplot2::vars(AUID_ATTNS),
-                 rows = ggplot2::vars(TADA.CharacteristicName),
-                 scales = "free_y") +
-      ggplot2::xlab('Time') +
-      ggplot2::ylab(ifelse(val_name > 1, 'Result', paste0(filt$TADA.CharacteristicName
-                                                 , ' ('
-                                                 , filt$TADA.ResultMeasure.MeasureUnitCode
-                                                 , ')'))) +
-      ggplot2::theme_bw() +
-      ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
-            panel.grid.minor = ggplot2::element_blank(),
-            strip.background = ggplot2::element_rect(fill = 'gray95'))
-  }
-
-  return(plt)
+  return(results)
 } # end if/else
 
