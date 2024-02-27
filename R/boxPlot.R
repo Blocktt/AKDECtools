805684f7f1f3d7c745214db3941ba48bab710652
ferribyh <ainsleyhf@gmail.com>
2024-02-27 11:01:34 -0500

    Added required fields

--- R/boxPlot.R
+++ R/boxPlot.R
@@ -8,10 +8,11 @@
 #' @details
 #' Required fields for input arguments
 #'
-#' * data:
-#' * WQS_table:
-#' * AU_ID:
-#' * y_axis_log:
+#' * data: TADA.CharacteristicName, AUID_ATTNS, TADA.ResultMeasureValue,
+#' MonitoringLocationIdentifier, TADA.ResultMeasure.MeasureUnitCode
+#' * WQS_table: TADA.Constituent
+#' * AU_ID: NA
+#' * y_axis_log: TRUE/FALSE
 #'
 #' @import magrittr
 #'
