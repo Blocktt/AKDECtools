3f53d889555e300e1cd2f387e0748becaaf0583a
ferribyh <ainsleyhf@gmail.com>
2024-05-21 15:45:59 -0400

    Add T/F input variable and changed for AK edits

--- R/categorize_AU.R
+++ R/categorize_AU.R
@@ -5,7 +5,8 @@
 #' for every applicable use.
 #'
 #' @param MagDurFreq_Results Final results from MagDurFreq_combine function.
-#'
+#' @param simplify_standards True/False input to condense final category for constituents
+#' with multiple standards per use.
 #' @examples
 #' # Example, data from Excel
 #'\dontrun{
@@ -28,27 +29,56 @@
 #' @return Categorized AUs
 #' @export
 #'
-categorize_AU <- function(MagDurFreq_Results){
+categorize_AU <- function(MagDurFreq_Results, simplify_standards){
 
-  calc_individual <- MagDurFreq_Results %>%
-    dplyr::mutate(Individual_Category = dplyr::case_when(Data_Sufficient == "No" ~ '3',
+  calc_individual <- input_analysis %>%
+    filter(Exceed != 'Requires manual analysis') %>%
+    filter(Exceed != 'AU not lake waters') %>%
+    filter(Exceed != 'Natural conditions less than or equal to 50 NTU') %>%
+    dplyr::mutate(Individual_Category = case_when(is.na(Data_Sufficient) ~ NA,
+                                                  Data_Sufficient == "No" ~ '3',
                                                   Exceed == 'Yes' ~ '5',
                                                   Exceed == 'No' ~ '2',
                                                   Exceed == 'Insufficient hardness' ~ '3',
                                                   Exceed == 'Insufficient dependent data' ~ '3',
                                                   T ~ NA))
 
-  calc_overall <- calc_individual %>%
+  if(simplify_standards == T){
+
+    mid_step <- calc_individual %>%
+      dplyr::group_by(AUID_ATTNS, Use, `Use Description`, TADA.CharacteristicName) %>%
+      dplyr::mutate(n = n(),
+                    is_2 = sum(ifelse(Individual_Category == '2', 1, 0)),
+                    is_3 = sum(ifelse(Individual_Category == '3', 1, 0)),
+                    is_5 = sum(ifelse(Individual_Category == '5', 1, 0)),
+                    #If n > 1, choose worse category
+                    new_Individual_Category = case_when(n > 1 & is_5 == 1 ~
+                                                          '5',
+                                                        n > 1 & is_5 == 0 & is_2 > 0 ~
+                                                          '2',
+                                                        T ~ Individual_Category)) %>%
+      dplyr::filter(Individual_Category == new_Individual_Category) %>%
+      dplyr::select(!c(Individual_Category, n, is_2, is_3, is_5)) %>%
+      dplyr::rename(Individual_Category = new_Individual_Category)
+
+
+  } else {
+    mid_step <- calc_individual
+  }
+
+
+  calc_overall <- mid_step %>%
     dplyr::group_by(AUID_ATTNS, Use) %>%
     dplyr::mutate(cat_5_present = length(Individual_Category[Individual_Category=='5']),
                   cat_2_present = length(Individual_Category[Individual_Category=='2']),
-                  Use_Category = dplyr::case_when(cat_5_present > 0 ~ '5',
+                  Use_Category = case_when(cat_5_present > 0 ~ '5',
                                            cat_5_present == 0 & cat_2_present > 0 ~ '2',
                                            cat_5_present == 0 & cat_2_present == 0 ~ '3',
-                                           T~NA)) %>%
+                                           T~ NA)) %>%
     dplyr::select(!c(cat_5_present, cat_2_present)) %>%
     dplyr::arrange(AUID_ATTNS, Use)
 
   return(calc_overall)
+
 
 } # end categorize_AU
